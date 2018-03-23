fe6c01d90c0cf8d2e8561d35c8780d454c437df4
James Sangalli <j.l.sangalli@gmail.com>
2018-03-23 23:47:41 +0800

    Create spreadfunds.sol

---
+++ contracts/current/spreadfunds.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.21;
+
+contract spreadfunds 
+{
+    function distributeFunds(address[] addresses, uint amt) public payable
+    {
+        uint num = msg.value / amt;
+        require(num == addresses.length);
+        for(uint i = 0; i < addresses.length; i++)
+        {
+            addresses[0].transfer(amt);    
+        }
+    }
+}
