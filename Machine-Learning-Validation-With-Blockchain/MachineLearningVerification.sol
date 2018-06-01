dcde4644c33d4ffe452c275b17873a1c35eed1dc
James Sangalli <j.l.sangalli@gmail.com>
2018-06-01 14:38:10 +0800

    fix race condition between endContract and payoutWinner

--- Machine-Learning-Validation-With-Blockchain/MachineLearningVerification.sol
+++ Machine-Learning-Validation-With-Blockchain/MachineLearningVerification.sol
@@ -1,53 +1,56 @@
 /*
     Creator: James Sangalli
     Purpose: the purpose of this contract is to create an incentive for machine
-    learning models to produce the correct known but manually generated outputs. 
-    
-    by hashing the correct output data and checking it against the authors hash 
-    (which is generated from the correct manual data), 
+    learning models to produce the correct known but manually generated outputs.
+
+    by hashing the correct output data and checking it against the authors hash
+    (which is generated from the correct manual data),
     someone who generates a correct output can claim the ether and has proven their 
     model works.
-    
-    Note: this is just a fun thought experiment. 
+
+    Note: this is just a fun thought experiment.
+
+    Example: classify the data appropriately, have a large dataset with the correct answers stored privately and hashed.
+    Model that successfully classifies the data will produce the correct hash and be able to take the prize.
 */
 
 pragma solidity ^0.4.17;
-contract MachineLearningVerification 
+contract MachineLearningVerification
 {
     address author;
     bytes32 hashOfCorrectOutput;
-    mapping(address => uint) contributions; 
-    uint expiry; 
+    mapping(address => uint) contributions;
+    uint expiry;
     //contribution must be burned to prevent author gaming the system
-    uint authorContribution; 
+    uint authorContribution;
     bool claimed = false;
     address[] contributors;
     address burnAddress = 0x000000000000000000000000000000000000dEaD;
     address winner;
     string winningFormula;
-    
-    modifier notExpired() 
+
+    modifier notExpired()
     {
         if (block.timestamp > expiry) revert();
         _;
     }
-    
-    modifier isSmallerThanAuthorContribution(uint newContribution) 
+
+    modifier isSmallerThanAuthorContribution(uint newContribution)
     {
         uint newSum = this.balance + newContribution;
         if (newSum > authorContribution) revert();
         _;
     }
-    
-    modifier notClaimed() 
+
+    modifier notClaimed()
     {
-        if(claimed == true) 
+        if(claimed == true)
         {
             revert();
         }
         _;
     }
-    
+
     constructor(bytes32 outputHash, uint expiryTimestamp) public payable
     {
         author = msg.sender;
@@ -56,12 +59,12 @@ contract MachineLearningVerification
         authorContribution = msg.value;
         burnAddress.transfer(msg.value);
     }
-    
+
     //if the correct hash is produced from the data of the correct output in a model
     //then the user gets ether.
     //this has issues, what stops the creator (who knows the correct output manually)
     //from taking all the ether?
-    //TODO use reputation or make it impossible for the creator to profit? 
+    //TODO use reputation or make it impossible for the creator to profit?
     //will use the latter first, maybe something else will come along
     //TODO prevent miner from cheating with this input, might require zksnarks
     function verifyInput(bytes32 outputHash, string formula) notExpired notClaimed public
@@ -70,24 +73,26 @@ contract MachineLearningVerification
         //else you would have to submit all the data and hash it (expensive)
         //also should be specific to the contract
         bytes32 hashOfValue = keccak256(outputHash, this);
-        if(hashOfValue == hashOfCorrectOutput) 
+        if(hashOfValue == hashOfCorrectOutput)
         {
             claimed = true;
             winningFormula = formula;
         }
     }
-    
+
     //if author is satisifed with the winning forumla he can approve the payout
     //author should be able to reproduce valid outputs using the formula
-    //and potentially be able to apply it to other data sets 
+    //and potentially be able to apply it to other data sets
     function payoutWinner() public
     {
         require(msg.sender == author);
         require(winner != address(0));
-        winner.transfer(this.balance);
+        require(claimed);
+        //winner gets all the ether in the contract and the contract ends
+        selfdestruct(winner);
     }
-    
-    function addContribution() payable public 
+
+    function addContribution() payable public
         isSmallerThanAuthorContribution(msg.value) notExpired notClaimed
     {
         bool contributor = false;
@@ -95,23 +100,23 @@ contract MachineLearningVerification
         {
             if(msg.sender == contributors[i]) contributor = true;
         }
-        contributions[msg.sender] += msg.value;    
+        contributions[msg.sender] += msg.value;
         if(contributor == false)
         {
             contributors.push(msg.sender);
         }
     }
-    
-    function increaseAuthorContribution() payable public notExpired 
+
+    function increaseAuthorContribution() payable public notExpired
     {
         require(msg.sender == author);
         authorContribution += msg.value;
         burnAddress.transfer(msg.value);
     }
-    
-    function endContract() public 
+
+    function endContract() public notExpired
     {
-        require(block.timestamp > expiry || claimed); 
+        require(!claimed);
         for(uint i = 0; i < contributors.length; i++)
         {
             //refund contributors
@@ -119,5 +124,5 @@ contract MachineLearningVerification
         }
         selfdestruct(author);
     }
-    
+
 }
