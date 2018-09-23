98a89bed83f354b45ef0766b026e921492945ab2
James Sangalli <j.l.sangalli@gmail.com>
2018-09-23 14:33:16 +0800

    handle relaying from processTransaction, refactor and add getters

--- Finance/bitcoin-to-ethereum-swap/BTC2ETH.sol
+++ Finance/bitcoin-to-ethereum-swap/BTC2ETH.sol
@@ -3,10 +3,10 @@ import "./BtcParser" as BtcParser;
 
 pragma solidity ^0.4.0;
 // "0x0000000000000005BE086099E0FF00FC0CFBC77A8DD09375AE889FBD259A0367", "0x85af7e7A6F15874C139695d6d8DC276a39c2d601", 30, 100
-//mainnet: 0x30B87c4fe7F12A955E9d44EafB16DceF18B1bF41
+//mainnet: 0x7c81DF31BB2f54f03A56Ab25c952bF3Fa39bDF46
 contract BTC2ETH is BtcParser, btcrelayInterface
 {
-    address _btcrelayAddress;
+    address public btcrelayAddress;
     bytes32[] claimedTxs;
     address admin;
     address paymaster;
@@ -22,8 +22,8 @@ contract BTC2ETH is BtcParser, btcrelayInterface
         paymaster = msg.sender;
         bitcoinAddress = btcAddress;
         //default mainnet
-        _btcrelayAddress = 0x41f274c0023f83391DE4e0733C609DF5a124c3d4;
-        btcrelay = btcrelayInterface(_btcrelayAddress);
+        btcrelayAddress = 0x41f274c0023f83391DE4e0733C609DF5a124c3d4;
+        btcrelay = btcrelayInterface(btcrelayAddress);
         ether2BitcoinRate = initialRate;
         feeRatio = initialFeeRatio;
     }
@@ -39,10 +39,26 @@ contract BTC2ETH is BtcParser, btcrelayInterface
         return feeRatio;
     }
 
+    function replacePaymaster(address newPaymaster) public
+    {
+        require(msg.sender == admin);
+        paymaster = newPaymaster;
+    }
+
     //admin/paymaster tops up the contract here
     function() public payable
     {
         require(msg.sender == admin || msg.sender == paymaster);
+    }
+
+    function getRelayAddress() public returns(address)
+    {
+        return btcrelayAddress;
+    }
+
+    function getBitcoinAddress() public returns(bytes20)
+    {
+        return bitcoinAddress;
     }
 
     function setEther2BitcoinPrice(uint rate) public
@@ -62,55 +78,25 @@ contract BTC2ETH is BtcParser, btcrelayInterface
         return ether2BitcoinRate;
     }
 
-    // rawTransaction - raw bytes of the transaction
-    // transactionIndex - transaction's index within the block, as int256
-    // merkleSibling - array of the sibling hashes comprising the Merkle proof, as int256[]
-    // blockHash - hash of the block that contains the transaction, as int256
+    //called by btcrelay contract on successful verification of a relayed transaction
     //uses the public key of the bitcoin address to generate the address
     //of the corresponding ether address
     //the same private key can claim the ether as was used to send the bitcoin
-    function bitcoin2EthereumSwap(
-        bytes rawTransaction,
-        int256 transactionIndex,
-        int256[] merkleSibling,
-        int256 blockHash
-    ) public
+    function processTransaction(bytes rawTransaction, uint256 transactionHash) public returns (int256)
     {
-        //verify transaction, if valid add it to the list
-        //derive the corresponding ethereum address by getting the public key
-        //of the bitcoin sender and casting it to the address
-        //pay out the amount of eth to the address applied by the daily rate
+        require(msg.sender == btcrelayAddress);
         bytes32 hashedRawTx = keccak256(rawTransaction);
-
         checkClaims(claimedTxs, hashedRawTx);
-
-        uint256 response = btcrelay.verifyTx(
-            rawTransaction,
-            transactionIndex,
-            merkleSibling,
-            blockHash
-        );
-
-        require(response > 0); //returns 0 if nothing found
-        bytes20 senderPubKey = getSenderPub(rawTransaction, blockHash);
+        bytes20 senderPubKey = getSenderPub(rawTransaction);
         address sender = address(keccak256(senderPubKey));
         var (amt1, address1, amt2, address2) = btcParser.getFirstTwoOutputs(rawTransaction);
         uint amountToTransfer = amt1 * ether2BitcoinRate;
-        makeTransfers(amountToTransfer, sender, rawTransaction, blockHash);
-    }
-
-    //need to split up functions else the stack will run out
-    function makeTransfers(
-        uint amountToTransfer,
-        address sender,
-        bytes rawTransaction,
-        int256 blockHash) internal
-    {
         uint feeToAdmin = amountToTransfer / feeRatio;
         sender.transfer(amountToTransfer - feeToAdmin);
         claimedTxs.push(keccak256(rawTransaction));
         //admin gets a fee for providing service and liquidity
         admin.transfer(feeToAdmin);
+        return int256(amountToTransfer);
     }
 
     function checkClaims(bytes32[] claimTxs, bytes32 hashedRawTx) internal
@@ -121,7 +107,8 @@ contract BTC2ETH is BtcParser, btcrelayInterface
         }
     }
 
-    function getSenderPub(bytes rawTransaction, int256 blockHash) returns(bytes20)
+    //requires that the first input went to our btcAddress
+    function getSenderPub(bytes rawTransaction) internal returns(bytes20)
     {
         var (amt1, address1, amt2, address2) = btcParser.getFirstTwoOutputs(rawTransaction);
         require(address1 == bitcoinAddress); //first output goes to us, second is change
