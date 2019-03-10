8b477a199dd0560216ab2e6c632706c75be6b144
James Sangalli <j.l.sangalli@gmail.com>
2019-03-10 20:19:10 +1100

    refactor message encoding in erc20Swap and safely check transferFrom

--- Finance/swaps/erc20Swap.sol
+++ Finance/swaps/erc20Swap.sol
@@ -15,41 +15,17 @@ contract erc20Swap {
         uint expiry,
         uint amount,
         address erc20ContractAddress,
-        uint8 v, 
+        uint8 v,
         bytes32 r,
         bytes32 s
-    ) public payable returns(bool) {
-        bytes32 message = formMessage(amount, expiry, msg.value, erc20ContractAddress);
+    ) public payable {
+        bytes32 message = keccak256(abi.encodePacked(amount, expiry, msg.value, erc20ContractAddress));
         address seller = ecrecover(message, v, r, s);
         erc20 erc20Contract = erc20(erc20ContractAddress);
-        require(erc20Contract.allowance(seller, address(this)) >= amount);
+        require(erc20Contract.transferFrom(seller, msg.sender, amount));
         require(claimedSignatures[r] != seller);
         //don't allow the deal to be done multiple times
         claimedSignatures[r] = seller;
-        return erc20Contract.transferFrom(seller, msg.sender, amount);
     }
-
-    function formMessage(
-        uint amount,
-        uint expiry,
-        uint price,
-        address contractAddress
-    ) internal pure returns(bytes32) {
-        bytes memory message = new bytes(105);
-        for(uint i = 0; i < 32; i++) {
-            message[i] = byte(bytes32(amount << (8 * i)));
-        }
-        for(uint i = 0; i < 32; i++) {
-            message[i + 32] = byte(bytes32(expiry << (8 * i)));
-        }
-        for(uint i = 0; i < 32; i++) {
-            message[i + 64] = byte(bytes32(price << (8 * i)));
-        }
-        for(uint i = 0; i < 20; i++) {
-            message[i + 96] = byte(bytes20(contractAddress) << (8 * i));
-        }
-        return keccak256(message);
-    }
-
 
 }
