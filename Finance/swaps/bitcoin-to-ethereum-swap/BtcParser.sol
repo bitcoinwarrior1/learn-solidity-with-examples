090985ab53dc24dcc4a8fd75968b96841d6ce1af
James Sangalli <j.l.sangalli@gmail.com>
2020-08-14 11:48:53 +1000

    WIP add buy eth tokens with BTC contract

--- Finance/swaps/bitcoin-to-ethereum-swap/BtcParser.sol
+++ Finance/swaps/bitcoin-to-ethereum-swap/BtcParser.sol
@@ -97,14 +97,14 @@
 // redeem script. The P2SH address is derived from the scriptHash.
 // Addresses are the scriptHash with a version prefix of 5, encoded as
 // Base58check. These addresses begin with a '3'.
-
+pragma solidity ^0.5.10;
 // parse a raw bitcoin transaction byte array
 library BtcParser {
     // Convert a variable integer into something useful and return it and
     // the index to after it.
-    function parseVarInt(bytes txBytes, uint pos) returns (uint, uint) {
+    function parseVarInt(bytes memory txBytes, uint pos) public returns (uint, uint) {
         // the first byte tells us how big the integer is
-        var ibit = uint8(txBytes[pos]);
+        uint8 ibit = uint8(txBytes[pos]);
         pos += 1;  // skip ibit
 
         if (ibit < 0xfd) {
@@ -118,32 +118,32 @@ library BtcParser {
         }
     }
     // convert little endian bytes to uint
-    function getBytesLE(bytes data, uint pos, uint bits) returns (uint) {
+    function getBytesLE(bytes memory data, uint pos, uint bits) public returns (uint) {
         if (bits == 8) {
             return uint8(data[pos]);
         } else if (bits == 16) {
-            return uint16(data[pos])
-                 + uint16(data[pos + 1]) * 2 ** 8;
+            return uint16(uint8(data[pos]))
+            + uint16(uint8(data[pos + 1])) * 2 ** 8;
         } else if (bits == 32) {
-            return uint32(data[pos])
-                 + uint32(data[pos + 1]) * 2 ** 8
-                 + uint32(data[pos + 2]) * 2 ** 16
-                 + uint32(data[pos + 3]) * 2 ** 24;
+            return uint32(uint8(data[pos]))
+            + uint32(uint8(data[pos + 1])) * 2 ** 8
+            + uint32(uint8(data[pos + 2])) * 2 ** 16
+            + uint32(uint8(data[pos + 3])) * 2 ** 24;
         } else if (bits == 64) {
-            return uint64(data[pos])
-                 + uint64(data[pos + 1]) * 2 ** 8
-                 + uint64(data[pos + 2]) * 2 ** 16
-                 + uint64(data[pos + 3]) * 2 ** 24
-                 + uint64(data[pos + 4]) * 2 ** 32
-                 + uint64(data[pos + 5]) * 2 ** 40
-                 + uint64(data[pos + 6]) * 2 ** 48
-                 + uint64(data[pos + 7]) * 2 ** 56;
+            return uint64(uint8(data[pos]))
+            + uint64(uint8(data[pos + 1])) * 2 ** 8
+            + uint64(uint8(data[pos + 2])) * 2 ** 16
+            + uint64(uint8(data[pos + 3])) * 2 ** 24
+            + uint64(uint8(data[pos + 4])) * 2 ** 32
+            + uint64(uint8(data[pos + 5])) * 2 ** 40
+            + uint64(uint8(data[pos + 6])) * 2 ** 48
+            + uint64(uint8(data[pos + 7])) * 2 ** 56;
         }
     }
     // scan the full transaction bytes and return the first two output
     // values (in satoshis) and addresses (in binary)
-    function getFirstTwoOutputs(bytes txBytes) public
-             returns (uint, bytes20, uint, bytes20)
+    function getFirstTwoOutputs(bytes memory txBytes) public
+    returns (uint, bytes20, uint, bytes20)
     {
         uint pos;
         uint[] memory input_script_lens = new uint[](2);
@@ -159,27 +159,27 @@ library BtcParser {
         (output_values, script_starts, output_script_lens, pos) = scanOutputs(txBytes, pos, 2);
 
         for (uint i = 0; i < 2; i++) {
-            var pkhash = parseOutputScript(txBytes, script_starts[i], output_script_lens[i]);
+            bytes20 pkhash = parseOutputScript(txBytes, script_starts[i], output_script_lens[i]);
             output_addresses[i] = pkhash;
         }
 
         return (output_values[0], output_addresses[0],
-                output_values[1], output_addresses[1]);
+        output_values[1], output_addresses[1]);
     }
     // Check whether `btcAddress` is in the transaction outputs *and*
     // whether *at least* `value` has been sent to it.
-    function checkValueSent(bytes txBytes, bytes20 btcAddress, uint value)
-             returns (bool)
+    function checkValueSent(bytes memory txBytes, bytes20 btcAddress, uint value) public
+    returns (bool)
     {
         uint pos = 4;  // skip version
         (, pos) = scanInputs(txBytes, pos, 0);  // find end of inputs
 
         // scan *all* the outputs and find where they are
-        var (output_values, script_starts, output_script_lens,) = scanOutputs(txBytes, pos, 0);
+        (uint[] memory output_values, uint[] memory script_starts, uint[] memory output_script_lens,) = scanOutputs(txBytes, pos, 0);
 
         // look at each output and check whether it at least value to btcAddress
         for (uint i = 0; i < output_values.length; i++) {
-            var pkhash = parseOutputScript(txBytes, script_starts[i], output_script_lens[i]);
+            bytes20 pkhash = parseOutputScript(txBytes, script_starts[i], output_script_lens[i]);
             if (pkhash == btcAddress && output_values[i] >= value) {
                 return true;
             }
@@ -190,8 +190,8 @@ library BtcParser {
     // of the inputs.
     // takes a 'stop' argument which sets the maximum number of
     // outputs to scan through. stop=0 => scan all.
-    function scanInputs(bytes txBytes, uint pos, uint stop) public
-             returns (uint[], uint)
+    function scanInputs(bytes memory txBytes, uint pos, uint stop) public
+    returns (uint[] memory, uint)
     {
         uint n_inputs;
         uint halt;
@@ -207,7 +207,7 @@ library BtcParser {
 
         uint[] memory script_lens = new uint[](halt);
 
-        for (var i = 0; i < halt; i++) {
+        for (uint8 i = 0; i < halt; i++) {
             pos += 36;  // skip outpoint
             (script_len, pos) = parseVarInt(txBytes, pos);
             script_lens[i] = script_len;
@@ -221,8 +221,8 @@ library BtcParser {
     // end position of the outputs.
     // takes a 'stop' argument which sets the maximum number of
     // outputs to scan through. stop=0 => scan all.
-    function scanOutputs(bytes txBytes, uint pos, uint stop) public
-             returns (uint[], uint[], uint[], uint)
+    function scanOutputs(bytes memory txBytes, uint pos, uint stop) public
+    returns ( uint[] memory, uint[] memory, uint[] memory, uint)
     {
         uint n_outputs;
         uint halt;
@@ -240,7 +240,7 @@ library BtcParser {
         uint[] memory script_lens = new uint[](halt);
         uint[] memory output_values = new uint[](halt);
 
-        for (var i = 0; i < halt; i++) {
+        for (uint i = 0; i < halt; i++) {
             output_values[i] = getBytesLE(txBytes, pos, 64);
             pos += 8;
 
@@ -253,52 +253,52 @@ library BtcParser {
         return (output_values, script_starts, script_lens, pos);
     }
     // Slice 20 contiguous bytes from bytes `data`, starting at `start`
-    function sliceBytes20(bytes data, uint start) public returns (bytes20) {
+    function sliceBytes20(bytes memory data, uint start) public returns (bytes20) {
         uint160 slice = 0;
         for (uint160 i = 0; i < 20; i++) {
-            slice += uint160(data[i + start]) << (8 * (19 - i));
+            slice += uint160(uint8(data[i + start])) << (8 * (19 - i));
         }
         return bytes20(slice);
     }
     // returns true if the bytes located in txBytes by pos and
     // script_len represent a P2PKH script
-    function isP2PKH(bytes txBytes, uint pos, uint script_len) returns (bool) {
+    function isP2PKH(bytes memory txBytes, uint pos, uint script_len) public returns (bool) {
         return (script_len == 25)           // 20 byte pubkeyhash + 5 bytes of script
-            && (txBytes[pos] == 0x76)       // OP_DUP
-            && (txBytes[pos + 1] == 0xa9)   // OP_HASH160
-            && (txBytes[pos + 2] == 0x14)   // bytes to push
-            && (txBytes[pos + 23] == 0x88)  // OP_EQUALVERIFY
-            && (txBytes[pos + 24] == 0xac); // OP_CHECKSIG
+        && (txBytes[pos] == 0x76)       // OP_DUP
+        && (txBytes[pos + 1] == 0xa9)   // OP_HASH160
+        && (txBytes[pos + 2] == 0x14)   // bytes to push
+        && (txBytes[pos + 23] == 0x88)  // OP_EQUALVERIFY
+        && (txBytes[pos + 24] == 0xac); // OP_CHECKSIG
     }
     // returns true if the bytes located in txBytes by pos and
     // script_len represent a P2SH script
-    function isP2SH(bytes txBytes, uint pos, uint script_len) returns (bool) {
+    function isP2SH(bytes memory txBytes, uint pos, uint script_len) public returns (bool) {
         return (script_len == 23)           // 20 byte scripthash + 3 bytes of script
-            && (txBytes[pos + 0] == 0xa9)   // OP_HASH160
-            && (txBytes[pos + 1] == 0x14)   // bytes to push
-            && (txBytes[pos + 22] == 0x87); // OP_EQUAL
+        && (txBytes[pos + 0] == 0xa9)   // OP_HASH160
+        && (txBytes[pos + 1] == 0x14)   // bytes to push
+        && (txBytes[pos + 22] == 0x87); // OP_EQUAL
     }
     // Get the pubkeyhash / scripthash from an output script. Assumes
     // pay-to-pubkey-hash (P2PKH) or pay-to-script-hash (P2SH) outputs.
     // Returns the pubkeyhash/ scripthash, or zero if unknown output.
-    function parseOutputScript(bytes txBytes, uint pos, uint script_len)
-             returns (bytes20)
+    function parseOutputScript(bytes memory txBytes, uint pos, uint script_len) public
+    returns (bytes20)
     {
         if (isP2PKH(txBytes, pos, script_len)) {
             return sliceBytes20(txBytes, pos + 3);
         } else if (isP2SH(txBytes, pos, script_len)) {
             return sliceBytes20(txBytes, pos + 2);
         } else {
-            return;
+            return bytes20(0);
         }
     }
     //NOTE: Only supports segwit txs
     //https://bitcoin.stackexchange.com/questions/79723/where-is-the-pubkey-for-segwit-inputs
     //if multisig, it will just grab the first pubkey
-    function getPubKeyFromTx(bytes txBytes) returns(bytes)
+    function getPubKeyFromTx(bytes memory txBytes) public returns(bytes memory)
     {
         uint pos = 0;
-        bytes pubkey;
+        bytes memory pubkey;
         for(uint i = 0; i < txBytes.length; i++)
         {
             //byte with value 0x21 is used to show the start of the pubkey in the raw tx
@@ -309,7 +309,7 @@ library BtcParser {
             }
         }
         uint index = 0;
-        for(i = pos; i < pos + 33; i++)
+        for(uint i = pos; i < pos + 33; i++)
         {
             pubkey[index] = txBytes[i];
             index++;
@@ -317,10 +317,10 @@ library BtcParser {
         return pubkey;
     }
 
-    function getSegtwitSignature(bytes txBytes) returns(bytes)
+    function getSegtwitSignature(bytes memory txBytes) public returns(bytes memory)
     {
         uint pos = 0;
-        bytes signature;
+        bytes memory signature;
         for(uint i = 0; i < txBytes.length; i++)
         {
             //byte with value 0x47 is used to show the start of the signature in the raw tx
@@ -331,7 +331,7 @@ library BtcParser {
             }
         }
         uint index = 0;
-        for(i = pos; i < pos + 71; i++)
+        for(uint i = pos; i < pos + 71; i++)
         {
             signature[index] = txBytes[i];
             index++;
